93453390c151f2846c0353fef597e3366be32367
zain zoabi <82890692+zainz98@users.noreply.github.com>
2023-03-27 16:37:20 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import cv2 #استدعاء المكتبة
+PH1 = cv2.imread('basic.jpg')
+PH2 = cv2.imread('images/tow.jpg')
+
+if PH1.shape == PH2.shape :
+    #print('The image have same size and channels')
+    diff = cv2.subtract(PH1,PH2)
+    b ,g ,r  = cv2.split(diff)
+    if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) and cv2.countNonZero(r):
+        print('The images are completly Equal')
+else:
+    print('the images are not Equal')
